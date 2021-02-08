7b66ef6fd8f522f857b5ff21b1c533ce26e25367
lin.leo <lin.leo@hiero7.com>
2021-02-08 11:55:56 +0800

    add lesson13

---
+++ learn_golang_30_day/lesson13.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	i interface{}
+)
+
+func convert(i interface{}) {
+	switch t := i.(type) {
+	case int:
+		println("i is interger", t)
+	case string:
+		println("i is string", t)
+	case float64:
+		println("i is float64", t)
+	default:
+		println("type not found")
+	}
+}
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received1", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received2", msg2)
+			// case msg3 := <-c2:
+			// fmt.Println("received3", msg3)
+		}
+	}
+
+	i = 100
+	convert(i)
+	i = float64(45.55)
+	convert(i)
+	i = "foo"
+	convert(i)
+	convert(float32(10.0))
+
+	ch := make(chan int, 1)
+
+	ch <- 1
+	v := <-ch
+	fmt.Println("v", v)
+
+	select {
+	case <-ch:
+		fmt.Println("random 01")
+	case <-ch:
+		fmt.Println("random 02")
+	default:
+		fmt.Println("exit")
+	}
+}
---
+++ learn_golang_30_day/readme.md
@@ -0,0 +1,9 @@
+# 學習來自
+[初學 Golang 30 天系列](https://ithelp.ithome.com.tw/articles/10156438)
+
+1. lesson5 - 陣列
+2. lesson6 - Slice
+3. lesson13 - select
+4. 
+
+
